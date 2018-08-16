6850fde3d9ceb5e226f347511428698c010c32f0
kstwoak <kstwoak47@163.com>
2018-08-16 15:22:23 +0800

    简单的完成了index请求并没有进行完善需要后续处理

---
+++ controllers/index.go
@@ -0,0 +1,31 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+	"29q/day9/ihome/utils"
+)
+
+type IndexController struct {
+	beego.Controller
+}
+
+func (c *IndexController) Retdata(resp interface{}){
+	c.Data["json"] = resp
+	c.ServeJSON()
+}
+
+
+func (c *IndexController) GetIndex() {
+	//打印被调用的函数
+	beego.Info("---------------- GET  /api/v1.0/houses/index GetIndex() ------------------")
+	//创建返回空间
+	resp := make(map[string]interface{})
+	resp["errno"] = utils.RECODE_OK
+	resp["errmsg"] = utils.RecodeText(resp["errno"].(string))
+	//延迟调用发送给前端json数据
+	defer c.Retdata(resp)
+
+
+	return
+}
+
